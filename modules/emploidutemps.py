6335692282fd2d18d2a0a3b3415cdbfb4564f1c8
Nathanwolfy <nathan.ldgb@gmail.com>
2021-11-24 07:38:06 +0100

    création fichiers classes

---
+++ modules/emploidutemps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 07:36:47 2021
+
+@author: natha
+"""
+
+
---
+++ modules/profiles.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 07:36:27 2021
+
+@author: natha
+"""
+
+
