71bb10924971a342d8e457ac7301bdd75006b3e8
lfdart <louisfdartencet@gmail.com>
2021-11-24 09:47:50 +0100

    Création input client

--- client_patient.py
+++ client_patient.py
@@ -1,1 +1,14 @@
+#import Profil from
+
+nom=input("NOM : ")
+
+prenom=input("Prenom : ")
+
+idt=(nom, prenom)
+
+adresse=input("Adresse (Ville) : ")
+
+naissance=input("Date de naissance (jj/mm/aaaa) : ")
+
+mel=input("Mail : ")
 
