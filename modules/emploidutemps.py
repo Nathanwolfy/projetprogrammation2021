b74aec4e3c36f4add3af0cd09083b43d70be7c39
Vic-Eline <94950660+Vic-Eline@users.noreply.github.com>
2021-11-25 15:12:47 +0100

    Update emploidutemps.py

--- modules/emploidutemps.py
+++ modules/emploidutemps.py
@@ -6,44 +6,38 @@ Created on Wed Nov 24 07:36:47 2021
 """
 jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
 
-def horaire():
-    tranche_horaire = [] #un jour : renvoie [06:00, 06:15, ...]
-    for heure in range(6, 10):
-        for quart_heure in range(4):
-            if quart_heure == 0:
-                tranche_horaire.append('0' + str(heure) + ':' + '00')
-            else:
-                tranche_horaire.append('0' + str(heure) + ':' + str(quart_heure*15))
-    for heure in range(10, 24):
-        for quart_heure in range(4):
-            if quart_heure == 0:
-                tranche_horaire.append(str(heure) + ':' + '00')
-            else:
-                tranche_horaire.append(str(heure) + ':' + str(quart_heure*15))
-    return tranche_horaire
-
-class Date:
-    def __init__(self, jour, mois):
-        self.jour = jour
-        self.mois = mois
-    def __repr__(self):
-        return str(jour) + mois
-        
 class Heure:
-    def __init__(self, t_debut, t_fin):
-        self.t_debut = t_debut
-        self.t_fin = t_fin
+    def __init__(self, heure, minute):
+        self.heure = heure
+        self.minute = minute
     def __repr__(self):
-        return str('t_debut') + str('t_fin')
-    
-class Edt(Date, Heure):
+        return str(self.heure) + ':' + str(self.minute)
+
+
+class Edt:
     def __init__(self):
-        self.edt = [[jours[i], ] for i in range(7)] #le tableau emploi du temps [[tranche_horaire lundi], [tranche_horaire mardi], ...]
-        self.rdv = {} #input un rdv
-    def saisie(self):
-        date = Date(input('Date : '))
-        heure = Heure(input('Heure :'))
-        for (i, jour) in enumerate(jours):
-            if date == jour: 
-                edt[i] 
+        self.L = [[[0]]*18*4]*7 #[[#lundi [06:00, motif], [06:15, motif], ...], [#mardi [06:00], [0], ...], [], [], ...]
+    def is_empty(self, jour, heure_debut, motif):
+        horaire = Horaire(jour, heure_debut, motif)
+        if 
+
+
+
+def load_edt(fichier):
+    edt = Edt()
+    with open(fichier) as file:
+        for horaire in file: #Lundi 22 01 2021 08 00 motif
+            horaire.split()
+            jour = horaire[0]
+            heure_rdv = int(horaire[4])
+            minute_rdv = int(horaire[5])
+            heure_debut = Heure(heure_rdv, minute_rdv)
+            minute = minute_rdv//15 #06:15 -> minute = 1
+            motif = horaire[-1]
+            for i in range(7):
+                if jour == jours[i]:
+                    edt[i][(heure_rdv-6)*4+minute] = heure_debut #[[[0], [0], ..., [08:00]]]
+                    edt[i][(heure_rdv-6)*4+minute].append(motif) #[[[0], [0], ..., [08:00, motif]]]
+    return edt
+
 
