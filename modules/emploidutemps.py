0632fa7b0df056c5e2c9e7b5aba0db96f30971e7
Vic-Eline <94950660+Vic-Eline@users.noreply.github.com>
2021-11-28 16:20:22 +0100

    Update emploidutemps.py

--- modules/emploidutemps.py
+++ modules/emploidutemps.py
@@ -11,39 +11,55 @@ class Heure:
         self.heure = heure
         self.minute = minute
     def __repr__(self):
-        return str(self.heure) + ':' + str(self.minute)
+        if self.heure < 10:
+            if self.minute == 0:
+                return '0' + str(self.heure) + ':' + '0' + str(self.minute)
+            else:
+                return '0' + str(self.heure) + ':' + str(self.minute)
+        else:
+            if self.minute == 0: 
+                return str(self.heure) + ':' + '0' + str(self.minute)
+            else:
+                return str(self.heure) + ':' + str(self.minute)
 
 
 class Edt:
     def __init__(self):
-        self.edt = [[['']]*18*4]*7 #[[#lundi [06:00, motif], [06:15, motif], ...], [#mardi [06:00], [''], ...], [], [], ...]
-    def is_empty(self, jour, heure_debut, motif):
+        self.edt = [{} for i in range(7)] #[{#lundi 06:00 : motif, 06:15 : motif, ...}, {#mardi 06:00 : motif, ...}, ...]
+    def is_empty(self, jour, heure_debut):
         empty = True
-        L_heure, L_minute = heure_debut.split(':')
-        heure = L_heure[0]
-        minute = L_minute[0]
         for j in jours:
             if jour == jours[j]:
-                if self.edt[j][(heure-6)*4+minute] != '':
+                if self.edt[j][heure_debut] == '':
                     return False
         
-
-
 
 def load_edt(fichier):
     edt = Edt()
     with open(fichier) as file:
-        for horaire in file: #Lundi 22 01 2021 08 00 motif
+        heure_journee = file[0][4] # ? Heure du debut de la journee
+        minute_journee = file[0][5] # ? Minute du debut de la journee
+        horaire_debut_boucle_while = Heure(heure_journee, minute_journee)  #Heure du début de la journée
+        heure_fin_journee = file[-1][4]
+        minute_fin_journee = file[-1][5]
+        horaire_fin_boucle_while = Heure(heure_fin_journee, minute_fin_journee) #Heure de la fin de la journée
+        for horaire in file: #Lundi 22 01 2021 8 00 motif
             H = horaire.split()
             jour = H[0]
             heure_rdv = int(H[4])
             minute_rdv = int(H[5])
             heure_debut = Heure(heure_rdv, minute_rdv)
-            minute = minute_rdv//15 #06:15 -> minute = 1
             motif = H[-1]
             for i in range(7):
                 if jour == jours[i]:
-                    edt[i][(heure_rdv-6)*4+minute] = heure_debut #[[[''], [''], ..., [08:00]]]
-                    edt[i][(heure_rdv-6)*4+minute].append(motif) #[[[''], [''], ..., [08:00, motif]]]
+                    while horaire_debut_boucle_while < heure_debut and horaire_debut_boucle_while <= horaire_fin_boucle_while:
+                        edt[i][horaire_debut_boucle_while] = ''
+                        if minute_journee == 45:
+                            heure_journee += 1
+                            minute_debut_journee = 0
+                            horaire_debut_boucle_while = Heure(heure_journee + 1, minute_journee)
+                        else:
+                            minute_debut_journee += 15
+                    edt[i][heure_debut] = motif
     return edt
 
