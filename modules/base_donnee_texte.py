b7101a31bd0dcc7d0309ddd0930980b207a45bd2
Sipha-codeur <94955037+Sipha-codeur@users.noreply.github.com>
2021-11-29 08:53:23 +0100

    Update base_donnee_texte.py

--- modules/base_donnee_texte.py
+++ modules/base_donnee_texte.py
@@ -70,29 +70,59 @@ def recherche_patient(string):
         return "La recherche n'a pas aboutie"
     else:
         return recherche
-        
-    
-def creer_patient(prenom, nom, jour, mois, annee, mail, telephone, adresse):
-    """Cette fonction créé un nouveau patient et rentre ses elements dans la base de donée"""
-    fichier = open ("patients.txt", "a")
-    fichier.writelines(str(prenom) + " " + str(nom) + " " + str(jour) + " " + str(mois) + " " + str(annee) + " " + str(mail) + " " + str(telephone) + " " + str(adresse))
-    fichier.close()
 
 
-def supprimer_patient():
-    pass
-
-
-def modifier_patient(mail):
-    """but : modifier la ligne d'un patient à partir de son mail, qui est par définition
-    unique"""
-    indiceligne = 0
+def indice_ligne_patient(mail):
+    """cette fonction envoie -1 : le patient dont le mail à été rentré n'existe pas
+    c'est à dire on peut le créer, sinon cette fonction renvoie l'indice de la ligne
+    c'est à dire l'indice de la liste dans laquelle on peut stocker les lignes + 1"""
+    indiceligne = -1
     with open("patients.txt", "r") as f:
         lignes = f.readlines()
         for texte in lignes:
             l = texte.split()
             if l[5] == mail :
-                indiceligne = lignes.index(texte)
+                indiceligne = lignes.index(texte) + 1
+    return indiceligne
+
+
+def creer_patient(prenom, nom, jour, mois, annee, mail, telephone, adresse):
+    """Cette fonction créé un nouveau patient et rentre ses elements dans la base de donée, 
+    elle retournera une erreur si le patient s'est déjà enregistré avec ce mail"""
+    indligne = indice_ligne_patient(mail)
+    if indligne == -1:
+        fichier = open ("patients.txt", "a")
+        fichier.write("\n" + str(prenom) + " " + str(nom) + " " + str(jour) + " " + str(mois) + " " + str(annee) + " " + str(mail) + " " + str(telephone) + " " + str(adresse))
+        fichier.close()
+    else :
+        return "Un patient avec le même mail existe déjà sur notre application doctobélix"
+
+
+def supprimer_patient(mail):
+    """ce programme permet de supprimer un patient de la base de donnée en supprimant ses 
+    données également"""
+    indligne = indice_ligne_patient(mail)
+    if indligne == -1:
+        return "le patient que vous essayez de supprimer n'existe pas encore sur doctobélix"
+    else :
+        with open("patients.txt", "r") as f:
+            lignes = f.readlines()
+        lignes.pop(indligne - 1)
+        with open("patients.txt", "w") as f:
+            for i in range(len(lignes)):
+                f.write(lignes[i])
+    #Régler le problème du sautage de ligne et jpense il va falloir faire ça d'urgence mdr
+
+def modifier_patient(prenom, nom, jour, mois, annee, mail, telephone, adresse):
+    """but : modifier la ligne d'un patient à partir de son mail, qui est par définition
+    unique"""
+    indligne = indice_ligne_patient(mail)
+    if indligne == -1:
+        return "Le patient à modifier n'a pas été trouvé dans notre application doctobélix"
+    else:
+        supprimer_patient(mail)
+        creer_patient(prenom, nom, jour, mois, annee, mail, telephone, adresse)
+    #return indiceligne
         #En fait c'est presque plus rapide de supprimer tout et de refaire un
         #fichier avec TOUTES les lignes et voila je pense que c'est bien
         #aussi ajouter dans la fonction recherche l'utilisation de lire_bdd_patient(l)
@@ -112,9 +142,21 @@ if __name__ == "__main__" :
     fin_de_recherche = recherche_patient("25")
     #print(fin_de_recherche)
     
+    a = indice_ligne_patient("ed.pro@gmail.com")
+    #print(a)
+    b = indice_ligne_patient("jojo.bizarre@venture.com")
+    #print(b)
+    c = indice_ligne_patient("the.weng.06@gmail.com")
+    #print(c)
+    
     #creer_patient("joseph", "bellobitto", 5, 12, 2001, "jojo.bizarre@venture.com", "0684397515", "Lieu dit Le GENEPI, POILLEY, 76950, FRANCE")
     #ça fonctionne
-    #creer_patient("frigiel", "fandefanta", 21, 3, 2009, "fantacoeurfrigiel@yahoo.fr", "07846951", "1 rue samantha Davies, SABLES-OLONNES, 45500, FRANCE")
+    #d = creer_patient("frigiel", "fandefanta", 21, 3, 2009, "fantacoeurfrigiel@yahoo.fr", "07846951", "1 rue samantha Davies, SABLES-OLONNES, 45500, FRANCE")
+    #print(d)
     
-    a = modifier_patient("frigieletfluffy@gmail.de")
-    print (a)
+    #e = supprimer_patient("fantacoeurfrigiel@yahoo.fr")  
+    #print(e)
+    
+    #d = modifier_patient("ed.pro@gmail.com")
+    #print (d)
+
