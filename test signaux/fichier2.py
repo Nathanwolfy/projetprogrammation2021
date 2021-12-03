31604f207ec7eb442fcbf694f5d6499b25ed219e
Nathanwolfy <nathan.ldgb@gmail.com>
2021-12-03 09:04:39 +0100

    emit

--- test signaux/fichier2.py
+++ test signaux/fichier2.py
@@ -8,13 +8,13 @@ Created on Wed Nov 24 07:51:26 2021
 from PySide6.QtCore import Signal as Signal
 from communication import Interface
 
-def connexion():
-    print ("hi it's me fichier2")
+def connexion(text):
+    print(text)
     
 
 interface = Interface()
 
-interface.reponse.connect(connexion)
+interface.reponse.connect(lambda text : text)
 
 while True:
     pass
