c57bbc693a3ae6660210820beff4700af2feecb8
Nathanwolfy <nathan.ldgb@gmail.com>
2021-12-05 17:11:28 +0100

    premier test réussi avec un seul socket et cliebt

--- test signaux/test_server.py
+++ test signaux/test_server.py
@@ -1,16 +1,20 @@
+#coding:utf-8
 import socket
 
 host, port = ('',5566)
 
-socket = socket.socket(socket.AF_INT, socket.SOCK_STREAM) #création du socket avec constante
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #création du socket avec constante
 socket.bind((host, port)) #on associe le socket à l'adresse et au port
 
 print('server is booted')
 
 while True:
-    socket.listen(10) #on met en écoute le port, 10=valeur du nombre de tentatives
+    socket.listen() #on met en écoute le port, 10=valeur du nombre de tentatives
     conn, address = socket.accept() #on accepte les connexions
     print('server is listening')
 
+    data = conn.recv(1024) #réception et taille de buffer = 256,1024,2048..
+    data = data.decode('utf8') #on décode la data
+    print(data)
 conn.close() #on ferme la connexion
 socket.close() #on ferme le socket
