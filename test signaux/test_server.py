14432136952f7da09d17dbf1866fbbdf551c6875
Nathanwolfy <nathan.ldgb@gmail.com>
2021-12-05 16:51:06 +0100

    première init

---
+++ test signaux/test_server.py
@@ -0,0 +1,12 @@
+import socket
+
+host, port = ('',5566)
+
+socket = socket.socket(socket.AF_INT, socket.SOCK_STREAM) #création du socket avec constante
+socket.bind((host, port)) #on associe le socket à l'adresse et au port
+
+print('server is booted')
+
+while True:
+    socket.listen(10) #on met en écoute le port, 10=valeur du nombre de tentatives
+    conn, address = socket.accept() #on accepte les connexions
