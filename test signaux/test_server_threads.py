58d114292ec0363812810c045bf6e54fa99a77eb
Nathanwolfy <nathan.ldgb@gmail.com>
2021-12-06 11:51:21 +0100

    test threading

---
+++ test signaux/test_server_threads.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+import socket
+import threading
+
+class ThreadForClient(threading.Thread):
+    def __init__(self,conn):
+        super().__init__(self)
+        self.conn = conn
+
+    def run(self):
+        data = self.conn.recv(1024)
+        data = data.decode("utf8")
+        print(data)
+
+#----------------
+
+host, port = ('',5566)
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #création du socket avec constante
+socket.bind((host, port)) #on associe le socket à l'adresse et au port
+
+print('server is booted')
+
+while True:
+    socket.listen() #on met en écoute le port, 10=valeur du nombre de tentatives
+    conn, address = socket.accept() #on accepte les connexions
+    print(f'client : {conn},{address} is connected')
+
+    threadclient = ThreadForClient(conn)
+    threadclient.start()
+
+conn.close() #on ferme la connexion
+socket.close() #on ferme le socket
