712077473591c353089c13e2515f8b3b408fdbf2
Nathanwolfy <nathan.ldgb@gmail.com>
2021-12-08 17:05:58 +0100

    modifs testnet

--- client_patient.py
+++ client_patient.py
@@ -1,1 +1,16 @@
 import modules.patient as patient
+import socket
+
+host, port = ('localhost',5566)
+FORMAT = 'utf-8'
+
+#Création du socket
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socket.connect((host,port))
+print('Connecté au serveur...')
+
+#TODO système pour attendre la requête pour démarrer l'initialisation du choix
+
+choix_client = 'patient'.encode(FORMAT)
+socket.sendall(choix_client)
