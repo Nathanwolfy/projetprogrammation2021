ac13738625871dffc21530f2c70a42c431476fab
Vic-Eline <94950660+Vic-Eline@users.noreply.github.com>
2021-12-08 17:19:47 +0100

    Update emploidutemps.py

--- modules/emploidutemps.py
+++ modules/emploidutemps.py
@@ -1,14 +1,16 @@
 jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
-nb_jours_mois = {}
-for i in range(len(mois)): #{"Janvier" : 31, "Février" : 28, ...}
-    if i%2 == 0:
-        nb_jours_mois[mois[i]] = 31
+mois = [i for i in range(1, 13)] #[1 #Janvier, 2 #Février, ...]
+def nb_jours_dans_un_mois(mois, annee): 
+    if mois%2 == 0:
+        return 31
     else:
-        if mois[i] == "Février":
-            nb_jours_mois[mois[i]] = 28 #2022 n'est pas une année bissextile
+        if mois == 2: #Février
+            if annee % 4 == 0:
+                return 29 #année bissextile
+            else:
+                return 28 #année non bissextile
         else:
-            nb_jours_mois[mois[i]] = 30
+            return 30
 
 class Heure:
     def __init__(self, heure, minute):
@@ -44,6 +46,9 @@ class Jour: # ex: Lundi 2 Janvier = {06:00 : motif1, 08:45 : carreaux, ...}
         self.nb_jour = nb_jour
         self.mois = mois
         self.jour = {}
+    def ajouter(self, horaire, motif):
+        self.jour[horaire] = motif
+        return self.jour
     def pas_de_rdv(self, heure_debut):
         if self.jour[heure_debut] == '':
             return True
@@ -54,53 +59,46 @@ class Jour: # ex: Lundi 2 Janvier = {06:00 : motif1, 08:45 : carreaux, ...}
     def __repr__(self):
         return str(self.jour)
 
-class Semaine(Jour):
-    def __init__(self, ):
-        pass
-
-class Edt(Jour): # Une semaine
-    def __init__(self, semaine): # numéro de la semaine, ex : semaine 14 = 14
-        super.__init__()
-        self.semaine = semaine
-        self.edt = [self.jour for i in range(7)] #[{#lundi 06:00 : motif, 06:15 : motif, ...}, {#mardi 06:00 : motif, ...}, ...]
-        
 class Edt: # Une semaine, juste besoin du lundi
-    def __init__(self, lundi, nb_lundi, mois_lundi): #ex : Lundi 2 Janvier
+    def __init__(self, lundi, nb_lundi, mois_lundi, annee): #ex : Lundi 2 Janvier (=01)
+        self.annee = annee
         Lundi = Jour(lundi, nb_lundi, mois_lundi)
         Liste_jours_de_la_semaine = []
         Liste_jours_de_la_semaine.append(Lundi)
         nb_jour = nb_lundi
-        for m in mois:
-            if m == mois_lundi:
-               indice_mois = mois.index(m) 
         for i in range(6): #[Lundi 2 Janvier, Mardi 3 Janvier, Mercredi 4 Janvier, ...]
-            if nb_jour < nb_jours_mois[mois_lundi]:
+            if nb_jour < nb_jours_dans_un_mois(mois_lundi, self.annee):
                 nb_jour += 1
                 Liste_jours_de_la_semaine.append(Jour(jours[i], nb_jour, mois_lundi))
             else:
-                if mois_lundi == "Décembre":
-                    mois_lundi = "Janvier"
+                if mois_lundi == 12:
+                    mois_lundi = 1
                 else:
-                    indice_mois += 1
-                    mois_lundi = mois[indice_mois]
+                    mois_lundi += 1
                 nb_jour = 1
                 Liste_jours_de_la_semaine.append(Jour(jours[i], nb_jour, mois_lundi))
         self.edt = [Liste_jours_de_la_semaine[i].jour for i in range(7)] #[{#lundi 06:00 : motif, 06:15 : motif, ...}, {#mardi 06:00 : motif, ...}, ...]
+    def un_jour_de_edt(self, i):
+        return self.edt[i]
+    def modifier(self, i, heure, motif):
+        self.un_jour_de_edt(i).ajouter(heure, motif)
+        return self.edt
     def is_empty(self, le_jour, heure_debut):
-        for j in jours:
+        for j in range(len(jours)):
             if le_jour == jours[j]:
-                if self.edt[j].pas_de_rdv(heure_debut):
+                if self.un_jour_de_edt(j).pas_de_rdv(heure_debut):
                     return True
     def __repr__(self):
         return str(self.edt)
 
 
 def load_edt(fichier):
-    edt = Edt()
     with open(fichier) as file:
         for (i, horaire) in enumerate(file): #Lundi 22 01 2021 8 00 motif
             H = horaire.split()
             if i == 0:
+                edt = Edt(H[0], int(H[1]), int(H[2]), int(H[3]))
+                print(edt)
                 heure_journee = int(H[4]) # Heure du début de la journée
                 minute_journee = int(H[5]) # Minute du début de la journée
                 horaire_debut_boucle_while = Heure(heure_journee, minute_journee)  #Heure du début de la journée
@@ -111,15 +109,17 @@ def load_edt(fichier):
             motif = H[-1]
             for i in range(7):
                 if jour == jours[i]:
-                    while horaire_debut_boucle_while.compare(heure_debut):
-                        edt[i].jour[horaire_debut_boucle_while] = ''
+                    while horaire_debut_boucle_while.compare(heure_debut): #hor_deb_bou_whi < heure_debut
+                        edt[i][horaire_debut_boucle_while] = ''
                         if minute_journee == 45:
                             heure_journee += 1
                             minute_debut_journee = 0
                             horaire_debut_boucle_while = Heure(heure_journee, minute_journee)
                         else:
                             minute_debut_journee += 15
-                    edt[i].jour[heure_debut] = motif
+                            horaire_debut_boucle_while = Heure(heure_journee, minute_journee)
+                    if edt.is_empty(jour, horaire_debut_boucle_while):
+                        edt.modifier(i, heure_debut, motif)
                     print(edt)
     return edt
 
