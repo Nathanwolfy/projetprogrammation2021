a662dd39fccbd9f31376bdf25e2e9597df8b546a
lfdart <louisfdartencet@gmail.com>
2022-01-10 09:53:22 +0100

    modif groupe

--- IHM/.qt_for_python/uic/launcher.py
+++ IHM/.qt_for_python/uic/launcher.py
@@ -6,19 +6,25 @@ import Cpriserdv
 import DEdTPraticiens
 import fonctions
 
-a = Achoixdocuser
-b = Bconnexionouinscription
-c = Cpriserdv
-d = DEdTPraticiens
+Yp = InscriptionPat
+Yd = InscriptionDoc
+Ig = Achoixdocuser
+IIg = Bconnexionouinscription
+IIIp = Cpriserdv
+IVp = DEdTPraticiens
 f = fonctions
+#rentrer ca dans le dico
 
 seq={1:a, 2:b, 2:c, 3:c, 4:d}
 
-def sequence(i):
+def sequence(i, arg):
     appS = QtWidgets.QApplication(sys.argv)
     widget = QtWidgets.QWidget()
-    mycustomwidget = seq[i].Ui_Form()
+    mycustomwidget = seq[i].Ui_Form(arg)
     mycustomwidget.setupUi(widget)
     widget.show()
-    appS.exec()       
+    appS.exec()  
+
+     
     
+sequence(3)
