8d27b02d779896a046892c7ffc10c02b39cf1f79
Nathanwolfy <nathan.ldgb@gmail.com>
2022-01-12 14:09:41 +0100

    ok

--- client.py
+++ client.py
@@ -27,8 +27,21 @@ choix_client = fonctions.Ametier() #'XXp' ou 'XXd'
 choix_client_encode = choix_client.encode(FORMAT)
 
 if choix_client == 'XXp':
+    
     socket.sendall(choix_client_encode)
     patient.client_patient(socket)
+
+    confirmation_serveur = socket.recv(32)
+    confirmation_serveur = confirmation_serveur.decode(FORMAT)
+
+    if confirmation_serveur == '02pINITCONN':
+        print("Lancement de l'interface de connexion patient ...")
+        launcher.sequence('IIg',[0,0])
+        #TODO Proposer la création d'un identifiant de connexion
+        print('a')
+        dentifiant_patient = fonctions.Bidentifiant()
+        motdepasse_patient = fonctions.Bmotdepass()
+
 elif choix_client == 'XXd':
     socket.sendall(choix_client_encode)
     docteur.client_docteur(socket)
