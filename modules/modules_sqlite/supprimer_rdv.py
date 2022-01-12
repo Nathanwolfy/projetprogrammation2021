616c03a45c2174da3b2ae9b21751934c98f52b11
Vic-Eline <94950660+Vic-Eline@users.noreply.github.com>
2022-01-12 15:44:06 +0100

    Update supprimer_rdv.py

--- modules/modules_sqlite/supprimer_rdv.py
+++ modules/modules_sqlite/supprimer_rdv.py
@@ -9,19 +9,30 @@ def connection(bdd):
         print(e)
         print('La connexion n\'a pas pu être établie.')
 
-con_1 = connection('calendrier.db')
-cursor_1 = con_1.cursor()
+
 def nom_jour(jour, mois, annee):
-    cursor_1.execute('SELECT * FROM calendrier ')
+    con_1 = connection('calendrier.db')
+    cursor_1 = con_1.cursor()
+    cursor_1.execute('SELECT * FROM calendrier WHERE nb_jour=? AND mois_jour=? AND annee=?', (jour, mois, annee))
+    req = cursor_1.fetchone()
+    return req[1]
 
 con = connection('donnees.db')
 cursor = con.cursor()
 
 
 def supprimer_rdv_passe(jour, mois, annee):
-    ajd = e.Jour()
-    cursor.execute('SELECT * FROM rdv_dispos')
+    ajd = e.Jour(nom_jour(jour, mois, annee), jour, mois, annee)
+    cursor.execute('SELECT * FROM test_supprimer')
     rows = cursor.fetchall()
     liste_jour = []
     for row in rows:
-        
+        jour = e.Jour(nom_jour(row[0], row[1], row[2]), row[0], row[1], row[2])
+        if jour < ajd:
+            liste_jour.append((row[0], row[1], row[2]))
+    cursor.executemany('DELETE FROM test_supprimer WHERE jour=? AND mois=? AND annee=?', liste_jour)
+    con.commit()
+    con.close()
+    return 'Le rendez-vous a été supprimé'
+
+supprimer_rdv_passe(1, 3, 2022)
