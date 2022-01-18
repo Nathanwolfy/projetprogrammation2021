56644a34024d468f6282bec97e2f135027d784cd
Nathanwolfy <nathan.ldgb@gmail.com>
2022-01-18 15:13:19 +0100

    i

--- modules/modules_sqlite/construction_edt.py
+++ modules/modules_sqlite/construction_edt.py
@@ -1,5 +1,6 @@
 import sqlite3
 from . import lire_sql as lsql
+from . import rdv_dispo_pris
 
 
 def nom_jour(jour, mois, annee):
@@ -10,14 +11,15 @@ def nom_jour(jour, mois, annee):
     con_1.close()
     return req[1]
 
-def construction_edt(medecin, jour, mois, annee, heure, minute, motif, note):
+def construction_edt(nom_medecin, jour, mois, annee, heure, minute, motif, note):
+    identifiant_medecin = rdv_dispo_pris.profil_medecin_complet(nom_medecin)[0]
     con = lsql.connection_bdd()
     cursor = con.cursor()
     nom_du_jour = nom_jour(jour, mois, annee)
-    cursor.execute('SELECT * FROM rdv_dispos WHERE medecin=? AND jour=? AND mois=? AND annee=? AND heure=? AND minute=?', (medecin, jour, mois, annee, heure, minute))
+    cursor.execute('SELECT * FROM rdv_dispos WHERE medecin=? AND jour=? AND mois=? AND annee=? AND heure=? AND minute=?', (identifiant_medecin, jour, mois, annee, heure, minute))
     dispo = cursor.fetchone()[6]
     if dispo == 1:
         cursor.execute('SELECT * FROM rdvs WHERE motif=?', [motif])
         duree = cursor.fetchone()[2]
-        cursor.execute('INSERT INTO edt VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (medecin, nom_du_jour, jour, mois, annee, heure, minute, motif, duree, note))
+        cursor.execute('INSERT INTO edt VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (identifiant_medecin, nom_du_jour, jour, mois, annee, heure, minute, motif, duree, note))
     print('Le rendez-vou a été ajouté.') 
