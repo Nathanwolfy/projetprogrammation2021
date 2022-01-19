45cdc5f505b4168fca8f64df6bd01f04e1ed0455
Nathanwolfy <nathan.ldgb@gmail.com>
2022-01-19 18:28:22 +0100

    e

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ from modules.modules_IHM.IHM_en_Python import launcher
 from modules.modules_sqlite import lire_sql
 import hashlib
 
-
+FORMAT = 'utf-8'
 #a = lire_sql.liste_type_medecin()
 #launcher.sequence('Yd',a)
 
 #dico_type_rdv = lire_sql.dictionnaire_pour_qt()
 #launcher.sequence('IIIp',(dico_type_rdv,False))
-print(hashlib.sha1(b'mdp'))
+hash_mdp = hashlib.sha1('mdp'.encode(FORMAT))
+print(hash_mdp.hexdigest())
