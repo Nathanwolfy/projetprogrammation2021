86a7be0b0b1906586894134c37cafe8e52f1b995
Vic-Eline <94950660+Vic-Eline@users.noreply.github.com>
2022-01-19 10:43:22 +0100

    Update annuler_rdv.py

--- modules/modules_sqlite/annuler_rdv.py
+++ modules/modules_sqlite/annuler_rdv.py
@@ -13,9 +13,13 @@ con = connection()
 cursor = con.cursor
 
 def annuler_rdv(jour, mois, annee, heure, minute, medecin):
+    '''Remplace dispo = 0 en dispo = 1 sur la base de données rdv_dispos et supprime le rdv dans edt'''
     if not rdp.rdv_dispo(jour, mois, annee, heure, minute, medecin):
         cursor.execute('DELETE FROM rdv_dispos WHERE jour=? AND mois=? AND annee=? AND heure=? AND minute=? AND medecin=?', (jour, mois, annee, heure, minute, medecin))
         cursor.execute('INSERT INTO rdv_dispos VALUES (?, ?, ?, ?, ?, ?, ?)', (jour, mois, annee, heure, minute, medecin, 1))
+        cursor.execute('DELETE FROM edt WHERE jour=? AND mois=? AND annee=? AND heure=? AND minute=? AND medecin=?', (jour, mois, annee, heure, minute, medecin))
+        con.commit()
+        con.close()
     return 'Le rendez-vous a été annulé'
 
 
