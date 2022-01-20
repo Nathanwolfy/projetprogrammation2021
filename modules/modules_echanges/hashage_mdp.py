b6981d15c62624ad955e6fd1af6f8c5f3f590c83
Nathanwolfy <nathan.ldgb@gmail.com>
2022-01-20 14:18:18 +0100

    update sha256

--- modules/modules_echanges/hashage_mdp.py
+++ modules/modules_echanges/hashage_mdp.py
@@ -1,5 +1,9 @@
 import hashlib
 
 def hash_mdp(motdepasse):
-    hash_mdp = hashlib.sha1(motdepasse.encode("utf-8"))
+    hash_mdp = hashlib.sha256(motdepasse.encode("utf-8"))
     return hash_mdp.hexdigest()
+
+b = 'eb55654hhJ'
+a = hash_mdp(b)
+print(a)
