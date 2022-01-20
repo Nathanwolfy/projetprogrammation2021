0096d530cfbdac72428631beeeec72d0dbd7d935
Nathanwolfy <nathan.ldgb@gmail.com>
2022-01-20 11:10:04 +0100

    e

--- modules/modules_IHM/IHM_en_Python/launcher.py
+++ modules/modules_IHM/IHM_en_Python/launcher.py
@@ -10,18 +10,6 @@ from . import InscriptionDoc
 from . import InscriptionDocedt
 from . import InscriptionPat
 
-seq={'Yp':InscriptionPat, 'YdA':InscriptionDoc, 'YdB': InscriptionDocedt, 'Ig':Achoixdocuser, 'IIg':Bconnexionouinscription, 'IIIp':Cpriserdv, 'IVp' :DEdTPatient, 'IVd' : DEdTDoc, 'Vp': Erecap}
-
-"""
-appS = QtWidgets.QApplication(sys.argv)
-
-def sequence(i, arg=[0,0]):
-    widget = QtWidgets.QWidget()
-    mycustomwidget = seq[i].Ui_Form(arg)
-    mycustomwidget.setupUi(widget)
-    widget.show()
-    appS.exec()"""
-
 class Achoixdocuser_herit(Achoixdocuser.Ui_Form):
     def __init__(self):
         super().__init__()
