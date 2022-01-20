a60e8aeebfd3ea3ca3171f9e70e94774a115f105
Nathanwolfy <nathan.ldgb@gmail.com>
2022-01-20 14:28:01 +0100

    Bons petits réglages

--- modules/modules_echanges/hashage_mdp.py
+++ modules/modules_echanges/hashage_mdp.py
@@ -1,6 +1,9 @@
 import hashlib
 
 def hash_mdp(motdepasse):
+    """ (str) -> (str)
+    Fonction qui renvoie le hachage d'une string rentrée en paramètre.
+    """
     hash_mdp = hashlib.sha256(motdepasse.encode("utf-8"))
     return hash_mdp.hexdigest()
 
